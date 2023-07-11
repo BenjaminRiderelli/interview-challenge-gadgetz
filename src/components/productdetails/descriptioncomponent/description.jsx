548aebc9e8f97cb0775f18d67f99cb67ca39a41f
BenjaminRiderelli <riderelli.ben@gmail.com>
2023-07-11 17:20:59 +0200

    fixed details namefields

--- src/components/productdetails/descriptioncomponent/description.jsx
+++ src/components/productdetails/descriptioncomponent/description.jsx
@@ -3,9 +3,20 @@ import styles from "./description.module.css";
 
 const DESCRIPTION = ({ descriptionData }) => {
   const textFields = Object.keys(descriptionData).map((key) => {
+    let fixedTitle = "";
+    if (key === "primaryCamera") {
+      fixedTitle = "Primary camera";
+    } else if (key === "secondaryCmera") {
+      fixedTitle = "Secondary camera";
+    } else if (key === "displayResolution") {
+      fixedTitle = "Display resolution";
+    } else {
+      fixedTitle = key;
+    }
+
     return (
       <div key={key} className={styles.textField}>
-        <h3>{key}</h3>
+        <h3>{fixedTitle.charAt(0).toUpperCase() + fixedTitle.slice(1)}</h3>
         <p>
           {key === "price" ? "$" : ""}
           {descriptionData[key] ? descriptionData[key] : "N/A"}
